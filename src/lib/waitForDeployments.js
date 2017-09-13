92cc75a1631ea3b15d2178127d3613735a2db318
Marius Lundgård <marius.lundgard@nrk.no>
2017-09-13 15:29:47 +0200

    feat(marathonctl): Add initial delay of 1s

--- src/lib/waitForDeployments.js
+++ src/lib/waitForDeployments.js
@@ -2,38 +2,41 @@
 
 const Promise = require('bluebird')
 
-const delay = (ms) => {
-  return new Promise((resolve) => setTimeout(resolve, ms))
+const delay = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 const waitForDeployments = (apiClient, deploymentIds, timeoutMs = -1) => {
   const startTime = Date.now()
+  const initialDelayMs = 1000
   const delayMs = 500
 
   const check = () => {
-    return apiClient.getDeployments().then((data) => {
-      const ids = data.map((d) => d.id)
-      const found = []
+    return delay(initialDelayMs).then(() => {
+      return apiClient.getDeployments().then(data => {
+        const ids = data.map(d => d.id)
+        const found = []
 
-      ids.forEach((deploymentId) => {
-        if (deploymentIds.indexOf(deploymentId) > -1) {
-          found.push(deploymentId)
-        }
-      })
-
-      if (found.length > 0) {
-        return delay(delayMs).then(() => {
-          const duration = Date.now() - startTime
-
-          if (timeoutMs > -1 && duration > timeoutMs) {
-            return Promise.reject(new Error('Timed out'))
+        ids.forEach(deploymentId => {
+          if (deploymentIds.indexOf(deploymentId) > -1) {
+            found.push(deploymentId)
           }
-
-          return check()
         })
-      }
 
-      return true
+        if (found.length > 0) {
+          return delay(delayMs).then(() => {
+            const duration = Date.now() - startTime
+
+            if (timeoutMs > -1 && duration > timeoutMs) {
+              return Promise.reject(new Error('Timed out'))
+            }
+
+            return check()
+          })
+        }
+
+        return true
+      })
     })
   }
 
